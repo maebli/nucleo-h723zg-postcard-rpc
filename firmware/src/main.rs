80ab3b2a3f2f658816d51074c056741930ab4d54
Michael Aebli <doland@duck.com>
2025-05-04 20:30:22 +0200

    refactoring

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,18 +1,20 @@
 #![no_std]
 #![no_main]
 
-use defmt::{info, trace};
+use defmt::{info, println, trace};
 use embassy_executor::Spawner;
-use embassy_stm32::adc::{Adc, SampleTime };
+use embassy_stm32::adc::{Adc, SampleTime};
 use embassy_stm32::rcc::mux::Adcsel;
 use embassy_time::Delay;
 use embedded_hal_1::delay::DelayNs;
 use {defmt_rtt as _, panic_probe as _};
 
+const VREFINT_MV: u32 = 1216; // mV DS13313 Rev 4
+const T_SAMPLING_VREFINT_MIN_NS: u32 = 430; // us DS13313 Rev 4
+const T_STARTUP_VREFINT_MAX_NS: u32 = 440; // us DS13313 Rev 4
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    // dfmt trace
-    trace!("Starting up...");
     let mut peripheral_config = embassy_stm32::Config::default();
     {
         peripheral_config.rcc.mux.adcsel = Adcsel::PER;
@@ -21,58 +23,22 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(peripheral_config);
 
     let mut delay = Delay;
-
     let mut adc = Adc::new(p.ADC3);
 
-    adc.set_sample_time(SampleTime::CYCLES810_5);
-    let mut vrefint = adc.enable_vrefint();
-    let mut temp = adc.enable_temperature();
+    // adc clock = 32Mhz 
+    let sample_time_us = 810.5/32.0; 
+    assert!(sample_time_us >= T_SAMPLING_VREFINT_MIN_NS as f32 / 1000.0);
 
-    delay.delay_us(50);
+    adc.set_resolution(embassy_stm32::adc::Resolution::BITS12);
+    adc.set_sample_time(SampleTime::CYCLES32_5);
 
-    for _ in 0..8 {
-        adc.blocking_read(&mut vrefint);
-    }
+    let mut vrefint_channel = adc.enable_vrefint();
 
-
-    let vrefint_sample = adc.blocking_read(&mut vrefint);
-    let convert_to_millivolts = |sample: u16| {
-        // From http://www.st.com/resource/en/datasheet/DM00071990.pdf
-        // 6.3.24 Reference voltage
-        const VREFINT_MV: u32 = 1216; // mV
-
-        (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample))
-    };
-
-    let convert_to_celcius = |sample| {
-        // From http://www.st.com/resource/en/datasheet/DM00071990.pdf
-        // 6.3.22 Temperature sensor characteristics
-        const V30: i32 = 620; // mV
-        const AVG_SLOPE: f32 = 2.0; // mV/C
-
-        let sample_mv = convert_to_millivolts(sample) as i32;
-
-        let ts_cal1 = unsafe { *(0x1FF1_E820 as *const u16) } as i32; // @30 °C
-        let ts_cal2 = unsafe { *(0x1FF1_E840 as *const u16) } as i32; // @130 °C
-        (sample as i32 - ts_cal1) as f32 * (130.0 - 30.0) / (ts_cal2 - ts_cal1) as f32 + 30.0
-    };
-
-    info!("VrefInt: {}", vrefint_sample);
-    const MAX_ADC_SAMPLE: u16 = (1 << 12) - 1;
-    info!("VCCA: {} mV", convert_to_millivolts(MAX_ADC_SAMPLE));
+    delay.delay_ns(T_STARTUP_VREFINT_MAX_NS);
 
     loop {
+        let vrefint_sample = adc.blocking_read(&mut vrefint_channel);
+        info!("VrefInt: {}", vrefint_sample);
         delay.delay_ms(1000);
-
-        let v = adc.blocking_read(&mut temp);
-        info!("PC1: {} ({} mV)", v, convert_to_millivolts(v));
-
-        let v = adc.blocking_read(&mut temp);
-        let celcius = convert_to_celcius(v);
-        info!("Internal temp: {} ({} C)", v, celcius);
-
-        let v = adc.blocking_read(&mut vrefint);
-        info!("VrefInt: {}", v);
-        info!("Internal temp: {} ({} C)", v, celcius);
     }
 }
